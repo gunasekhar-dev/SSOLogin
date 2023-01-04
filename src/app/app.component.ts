6fa1e0f645d5ae56d3f5c6f30fad0d8b1d3868d5
gunasekhar-dev <80669471+gunasekhar-dev@users.noreply.github.com>
2023-01-04 14:37:01 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'SSOLogin';
+  constructor(private oauthService: OAuthService) {
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+  login() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+  get token() {
+    let claims: any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
 }
